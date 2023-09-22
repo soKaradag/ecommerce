d7f889c06a83f07cc08c4cb2ab6fc2b938cb960a
Serdar Onur Karadağ <98060048+soKaradag@users.noreply.github.com>
2023-09-23 01:54:26 +0300

    Added Infinity scroll and Glassmorphism Navbar

---
+++ src/DummyData/Dummy.js
@@ -0,0 +1,249 @@
+import Carrot from "../images/carrot.jpg";
+import Orange from "../images/orange.jpg";
+import Strawberry from "../images/strawberry.jpg";
+import Tomato from "../images/tomato.jpg";
+
+const items = [
+    {
+        name: "Carrot",
+        category: "Grocery",
+        price: 25.99,
+        image: Carrot,
+    },
+    {
+        name: "Orange",
+        category: "Grecory",
+        price: 19.99,
+        image: Orange,
+    },
+    {
+        name: "Strawberry",
+        category: "Grocery",
+        price: 25.99,
+        image: Strawberry,
+    },
+    {
+        name: "Tomato",
+        category: "Grecory",
+        price: 19.99,
+        image: Tomato,
+    },
+    {
+        name: "Carrot",
+        category: "Grocery",
+        price: 25.99,
+        image: Carrot,
+    },
+    {
+        name: "Orange",
+        category: "Grecory",
+        price: 19.99,
+        image: Orange,
+    },
+    {
+        name: "Strawberry",
+        category: "Grocery",
+        price: 25.99,
+        image: Strawberry,
+    },
+    {
+        name: "Tomato",
+        category: "Grecory",
+        price: 19.99,
+        image: Tomato,
+    },
+    {
+        name: "Carrot1",
+        category: "Grocery",
+        price: 25.99,
+        image: Carrot,
+    },
+    {
+        name: "Orange1",
+        category: "Grecory",
+        price: 19.99,
+        image: Orange,
+    },
+    {
+        name: "Strawberry1",
+        category: "Grocery",
+        price: 25.99,
+        image: Strawberry,
+    },
+    {
+        name: "Tomato1",
+        category: "Grecory",
+        price: 19.99,
+        image: Tomato,
+    },
+    {
+        name: "Carrot1",
+        category: "Grocery",
+        price: 25.99,
+        image: Carrot,
+    },
+    {
+        name: "Orange1",
+        category: "Grecory",
+        price: 19.99,
+        image: Orange,
+    },
+    {
+        name: "Strawberry1",
+        category: "Grocery",
+        price: 25.99,
+        image: Strawberry,
+    },
+    {
+        name: "Tomato1",
+        category: "Grecory",
+        price: 19.99,
+        image: Tomato,
+    },
+    {
+        name: "Carrot",
+        category: "Grocery",
+        price: 25.99,
+        image: Carrot,
+    },
+    {
+        name: "Orange",
+        category: "Grecory",
+        price: 19.99,
+        image: Orange,
+    },
+    {
+        name: "Strawberry",
+        category: "Grocery",
+        price: 25.99,
+        image: Strawberry,
+    },
+    {
+        name: "Tomato",
+        category: "Grecory",
+        price: 19.99,
+        image: Tomato,
+    },
+    {
+        name: "Carrot",
+        category: "Grocery",
+        price: 25.99,
+        image: Carrot,
+    },
+    {
+        name: "Orange",
+        category: "Grecory",
+        price: 19.99,
+        image: Orange,
+    },
+    {
+        name: "Strawberry",
+        category: "Grocery",
+        price: 25.99,
+        image: Strawberry,
+    },
+    {
+        name: "Tomato",
+        category: "Grecory",
+        price: 19.99,
+        image: Tomato,
+    },
+    {
+        name: "Carrot",
+        category: "Grocery",
+        price: 25.99,
+        image: Carrot,
+    },
+    {
+        name: "Orange",
+        category: "Grecory",
+        price: 19.99,
+        image: Orange,
+    },
+    {
+        name: "Strawberry",
+        category: "Grocery",
+        price: 25.99,
+        image: Strawberry,
+    },
+    {
+        name: "Tomato",
+        category: "Grecory",
+        price: 19.99,
+        image: Tomato,
+    },
+    {
+        name: "Carrot",
+        category: "Grocery",
+        price: 25.99,
+        image: Carrot,
+    },
+    {
+        name: "Orange",
+        category: "Grecory",
+        price: 19.99,
+        image: Orange,
+    },
+    {
+        name: "Strawberry",
+        category: "Grocery",
+        price: 25.99,
+        image: Strawberry,
+    },
+    {
+        name: "Tomato",
+        category: "Grecory",
+        price: 19.99,
+        image: Tomato,
+    },
+    {
+        name: "Carrot",
+        category: "Grocery",
+        price: 25.99,
+        image: Carrot,
+    },
+    {
+        name: "Orange",
+        category: "Grecory",
+        price: 19.99,
+        image: Orange,
+    },
+    {
+        name: "Strawberry",
+        category: "Grocery",
+        price: 25.99,
+        image: Strawberry,
+    },
+    {
+        name: "Tomato",
+        category: "Grecory",
+        price: 19.99,
+        image: Tomato,
+    },
+    {
+        name: "Carrot",
+        category: "Grocery",
+        price: 25.99,
+        image: Carrot,
+    },
+    {
+        name: "Orange",
+        category: "Grecory",
+        price: 19.99,
+        image: Orange,
+    },
+    {
+        name: "Strawberry",
+        category: "Grocery",
+        price: 25.99,
+        image: Strawberry,
+    },
+    {
+        name: "Tomato",
+        category: "Grecory",
+        price: 19.99,
+        image: Tomato,
+    },
+    
+];
+export default items;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,320 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import Card from "./Card";
-import Carrot from "../images/carrot.jpg";
-import Orange from "../images/orange.jpg";
-import Strawberry from "../images/strawberry.jpg";
-import Tomato from "../images/tomato.jpg";
+import items from "../DummyData/Dummy";
 
 function Home() {
-    const items = [
-        {
-            name: "Carrot",
-            category: "Grocery",
-            price: 25.99,
-            image: Carrot,
-        },
-        {
-            name: "Orange",
-            category: "Grecory",
-            price: 19.99,
-            image: Orange,
-        },
-        {
-            name: "Strawberry",
-            category: "Grocery",
-            price: 25.99,
-            image: Strawberry,
-        },
-        {
-            name: "Tomato",
-            category: "Grecory",
-            price: 19.99,
-            image: Tomato,
-        },
-        {
-            name: "Carrot",
-            category: "Grocery",
-            price: 25.99,
-            image: Carrot,
-        },
-        {
-            name: "Orange",
-            category: "Grecory",
-            price: 19.99,
-            image: Orange,
-        },
-        {
-            name: "Strawberry",
-            category: "Grocery",
-            price: 25.99,
-            image: Strawberry,
-        },
-        {
-            name: "Tomato",
-            category: "Grecory",
-            price: 19.99,
-            image: Tomato,
-        },
-        {
-            name: "Carrot",
-            category: "Grocery",
-            price: 25.99,
-            image: Carrot,
-        },
-        {
-            name: "Orange",
-            category: "Grecory",
-            price: 19.99,
-            image: Orange,
-        },
-        {
-            name: "Strawberry",
-            category: "Grocery",
-            price: 25.99,
-            image: Strawberry,
-        },
-        {
-            name: "Tomato",
-            category: "Grecory",
-            price: 19.99,
-            image: Tomato,
-        },
-        {
-            name: "Carrot",
-            category: "Grocery",
-            price: 25.99,
-            image: Carrot,
-        },
-        {
-            name: "Orange",
-            category: "Grecory",
-            price: 19.99,
-            image: Orange,
-        },
-        {
-            name: "Strawberry",
-            category: "Grocery",
-            price: 25.99,
-            image: Strawberry,
-        },
-        {
-            name: "Tomato",
-            category: "Grecory",
-            price: 19.99,
-            image: Tomato,
-        },
-        {
-            name: "Carrot",
-            category: "Grocery",
-            price: 25.99,
-            image: Carrot,
-        },
-        {
-            name: "Orange",
-            category: "Grecory",
-            price: 19.99,
-            image: Orange,
-        },
-        {
-            name: "Strawberry",
-            category: "Grocery",
-            price: 25.99,
-            image: Strawberry,
-        },
-        {
-            name: "Tomato",
-            category: "Grecory",
-            price: 19.99,
-            image: Tomato,
-        },
-        {
-            name: "Carrot",
-            category: "Grocery",
-            price: 25.99,
-            image: Carrot,
-        },
-        {
-            name: "Orange",
-            category: "Grecory",
-            price: 19.99,
-            image: Orange,
-        },
-        {
-            name: "Strawberry",
-            category: "Grocery",
-            price: 25.99,
-            image: Strawberry,
-        },
-        {
-            name: "Tomato",
-            category: "Grecory",
-            price: 19.99,
-            image: Tomato,
-        },
-        {
-            name: "Carrot",
-            category: "Grocery",
-            price: 25.99,
-            image: Carrot,
-        },
-        {
-            name: "Orange",
-            category: "Grecory",
-            price: 19.99,
-            image: Orange,
-        },
-        {
-            name: "Strawberry",
-            category: "Grocery",
-            price: 25.99,
-            image: Strawberry,
-        },
-        {
-            name: "Tomato",
-            category: "Grecory",
-            price: 19.99,
-            image: Tomato,
-        },
-        {
-            name: "Carrot",
-            category: "Grocery",
-            price: 25.99,
-            image: Carrot,
-        },
-        {
-            name: "Orange",
-            category: "Grecory",
-            price: 19.99,
-            image: Orange,
-        },
-        {
-            name: "Strawberry",
-            category: "Grocery",
-            price: 25.99,
-            image: Strawberry,
-        },
-        {
-            name: "Tomato",
-            category: "Grecory",
-            price: 19.99,
-            image: Tomato,
-        },
-        {
-            name: "Carrot",
-            category: "Grocery",
-            price: 25.99,
-            image: Carrot,
-        },
-        {
-            name: "Orange",
-            category: "Grecory",
-            price: 19.99,
-            image: Orange,
-        },
-        {
-            name: "Strawberry",
-            category: "Grocery",
-            price: 25.99,
-            image: Strawberry,
-        },
-        {
-            name: "Tomato",
-            category: "Grecory",
-            price: 19.99,
-            image: Tomato,
-        },
-        {
-            name: "Carrot",
-            category: "Grocery",
-            price: 25.99,
-            image: Carrot,
-        },
-        {
-            name: "Orange",
-            category: "Grecory",
-            price: 19.99,
-            image: Orange,
-        },
-        {
-            name: "Strawberry",
-            category: "Grocery",
-            price: 25.99,
-            image: Strawberry,
-        },
-        {
-            name: "Tomato",
-            category: "Grecory",
-            price: 19.99,
-            image: Tomato,
-        },
-        
-    ];
+  const [sayfaNumarasi, setSayfaNumarasi] = useState(1);
+  const [gosterilenOgeler, setGosterilenOgeler] = useState([]);
 
-  const column1 = [];
-  const column2 = [];
-  const column3 = [];
-  const column4 = [];
+  useEffect(() => {
+    const yeniOgeler = items.slice(
+      (sayfaNumarasi - 1) * 8,
+      sayfaNumarasi * 8
+    );
 
-  for (let i = 0; i < items.length; i++) {
-    if (i % 4 === 0) {
-      column1.push(items[i]);
-    } else if (i % 4 === 1) {
-      column2.push(items[i]);
-    } else if (i % 4 === 2) {
-      column3.push(items[i]);
-    } else if (i % 4 === 3) {
-      column4.push(items[i]);
+    setGosterilenOgeler((prevOgeler) => [...prevOgeler, ...yeniOgeler]);
+  }, [sayfaNumarasi]);
+
+
+  useEffect(() => {
+    function handleScroll() {
+      if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight
+      ) {
+        setSayfaNumarasi((prevSayfaNumarasi) => prevSayfaNumarasi + 1);
+      }
     }
-  }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div className="Home">
       <div className="row">
-        <div className="column">
-          {column1.map((item, index) => (
+        {gosterilenOgeler.map((item, index) => (
+          <div className="column" key={index}>
             <Card
-              key={index}
               name={item.name}
               category={item.category}
               price={item.price}
               image={item.image}
             />
-          ))}
-        </div>
-        <div className="column">
-          {column2.map((item, index) => (
-            <Card
-              key={index}
-              name={item.name}
-              category={item.category}
-              price={item.price}
-              image={item.image}
-            />
-          ))}
-        </div>
-        <div className="column">
-          {column3.map((item, index) => (
-            <Card
-              key={index}
-              name={item.name}
-              category={item.category}
-              price={item.price}
-              image={item.image}
-            />
-          ))}
-        </div>
-        <div className="column">
-          {column4.map((item, index) => (
-            <Card
-              key={index}
-              name={item.name}
-              category={item.category}
-              price={item.price}
-              image={item.image}
-            />
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
